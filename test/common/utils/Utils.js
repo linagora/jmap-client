390d773cf6d67a949d783c56367f02c94473a352
Thomas Hilaire <thilaire@linagora.com>
2015-10-29 12:12:49 +0100

    OR-1891 assertRequiredParameterIsPresent should accept false

--- test/common/utils/Utils.js
+++ test/common/utils/Utils.js
@@ -15,10 +15,53 @@ describe('The Utils class', function() {
 
   });
 
+  describe('The isDefined static method', function() {
+
+    it('should return false for undefined', function() {
+      expect(jmap.Utils.isDefined(undefined)).to.equal(false);
+    });
+
+    it('should return false for null', function() {
+      expect(jmap.Utils.isDefined(null)).to.equal(false);
+    });
+
+    it('should return true for zero', function() {
+      expect(jmap.Utils.isDefined(0)).to.equal(true);
+    });
+
+    it('should return true for false', function() {
+      expect(jmap.Utils.isDefined(false)).to.equal(true);
+    });
+
+    it('should return true for empty string', function() {
+      expect(jmap.Utils.isDefined('')).to.equal(true);
+    });
+
+    it('should return true for empty array', function() {
+      expect(jmap.Utils.isDefined([])).to.equal(true);
+    });
+
+    it('should return true for object', function() {
+      expect(jmap.Utils.isDefined({})).to.equal(true);
+    });
+  });
+
   describe('The assertRequiredParameterIsPresent static method', function() {
 
     it('should not throw an Error if the parameter is defined', function() {
       expect(jmap.Utils.assertRequiredParameterIsPresent({})).to.deep.equal({});
+    });
+
+    it('should not throw an Error if the parameter is false', function() {
+      expect(jmap.Utils.assertRequiredParameterIsPresent(false, 'parameter')).to.equal(false);
+    });
+
+    it('should not throw an Error if the parameter is zero', function() {
+      expect(jmap.Utils.assertRequiredParameterIsPresent(0, 'parameter')).to.equal(0);
+    });
+
+    it('should not throw an Error if the parameter is empty string', function() {
+      expect(jmap.Utils.assertRequiredParameterIsPresent('', 'parameter')).to.equal('');
     });
 
     it('should throw an Error if the parameter is null', function() {
